99230ff56360752be9061125e0d37eb27966f1c5
Ken Karanja <ken@qenops.co.ke>
2025-05-05 00:55:55 +0300

    adjusted width property

--- src/components/properties/AddProperty.jsx
+++ src/components/properties/AddProperty.jsx
@@ -196,7 +196,7 @@ export default function AddProperty() {
   };
 
   return (
-    <div className="w-full max-w-sm md:max-w-md lg:max-w-xl xl:max-w-2xl mx-auto p-6 bg-white rounded shadow space-y-6">
+    <div className="w-full max-w-sm md:max-w-md xl:max-w-4xl mx-auto p-6 bg-white rounded shadow space-y-6">
       <h2 className="text-xl font-bold">Add New Property</h2>
 
       <form onSubmit={handleSubmit} className="space-y-6">
